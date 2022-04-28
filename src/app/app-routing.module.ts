949d89dd72d9083bd8af3e65861a9593163fc6ab
Arthur <arthurdlima99@gmail.com>
2022-04-27 21:10:13 -0300

    login update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContatoComponent } from './views/contato/contato.component';
-import { HomeComponent } from './views/home/home.component';
 import { ConsolesComponent } from './views/consoles/consoles.component';
 import { DesktopComponent } from './views/desktop/desktop.component';
 import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
 
 const routes: Routes = [
-  {path: "home", component: HomeComponent},
+  {path: "", component: HomeComponent},
   {path: "contato", component: ContatoComponent},
   {path: "console", component: ConsolesComponent},
   {path: "desktop", component:  DesktopComponent},
-  {path: "login", component:  LoginComponent},
-
+  {path: "login", component:  LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './views/home/home.component';
 import { ContatoComponent } from './views/contato/contato.component';
 import { HeaderComponent } from './views/header/header.component';
 import { DesktopComponent } from './views/desktop/desktop.component';
@@ -15,7 +14,6 @@ import { HttpClientModule } from '@angular/common/http';;
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
     ContatoComponent,
     HeaderComponent,
     DesktopComponent,
